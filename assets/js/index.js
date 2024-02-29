5c1fb31c14696987dbeeb94737e48dfd6ff8d781
DevaNanda <nandadeva870@gmail.com>
2024-02-29 20:50:43 +0700

    feat: responsive navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,32 +12,65 @@ window.addEventListener('scroll', function () {
 });
 
 const hamburgerLineOpen = {
-  first: ['top-[16px]','rotate-45'],
+  first: ['top-[16px]', 'rotate-45'],
   second: 'opacity-0',
-  third: ['bottom-3','-rotate-45']
+  third: ['bottom-3', '-rotate-45']
 }
 
+const navModalShow = "top-24"
+const navModalHidden = "-top-80"
+
 const hamburger = document.getElementById('hamburger');
+const navModal = document.getElementById('nav-modal');
 
 hamburger.addEventListener('mousedown', (e) => {
   const firstLine = document.getElementById('line-1')
   const secondLine = document.getElementById('line-2')
   const thirdLine = document.getElementById('line-3')
-  
-  if(!hamburger.classList.contains('navbar-open')) {
+
+  if (!hamburger.classList.contains('navbar-open')) {
     firstLine.classList.add(...hamburgerLineOpen.first)
     secondLine.classList.add(hamburgerLineOpen.second)
     thirdLine.classList.add(...hamburgerLineOpen.third)
     hamburger.classList.add('navbar-open')
+
+    navModal.classList.add(navModalShow)
   } else {
     firstLine.classList.remove(...hamburgerLineOpen.first)
     secondLine.classList.remove(hamburgerLineOpen.second)
     thirdLine.classList.remove(...hamburgerLineOpen.third)
     hamburger.classList.remove('navbar-open')
+    
+    navModal.classList.remove(navModalShow)
   }
 
 
 })
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(el => {
+  el.addEventListener('click', (e) => {
+    const firstLine = document.getElementById('line-1')
+    const secondLine = document.getElementById('line-2')
+    const thirdLine = document.getElementById('line-3')
+
+    if (!hamburger.classList.contains('navbar-open')) {
+      firstLine.classList.add(...hamburgerLineOpen.first)
+      secondLine.classList.add(hamburgerLineOpen.second)
+      thirdLine.classList.add(...hamburgerLineOpen.third)
+      hamburger.classList.add('navbar-open')
+      navModal.classList.add(navModalShow)
+    } else {
+      firstLine.classList.remove(...hamburgerLineOpen.first)
+      secondLine.classList.remove(hamburgerLineOpen.second)
+      thirdLine.classList.remove(...hamburgerLineOpen.third)
+      hamburger.classList.remove('navbar-open')
+      navModal.classList.remove(navModalShow)
+    }
+  })
+})
+
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -60,7 +93,7 @@ const swiper = new Swiper('.swiper', {
     1280: {
       slidesPerView: 4,
       spaceBetween: 40
-      
+
     }
   }
 });
